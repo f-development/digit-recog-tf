2d338021561dbffba19f7b5a97fc16413cdd2922
KEEWOONG NAM <keewoong.nam@flicspy.com>
2023-07-02 19:36:39 +0900

    Fix

--- cloudfront.tf
+++ cloudfront.tf
@@ -35,6 +35,12 @@ resource "aws_cloudfront_distribution" "this" {
     compress               = true
   }
 
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    ssl_support_method       = "sni-only"
+    minimum_protocol_version = "TLSv1.2_2021"
+  }
+  
   price_class = "PriceClass_All"
 
   restrictions {
