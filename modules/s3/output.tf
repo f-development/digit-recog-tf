9e96b7a8db4b8c0f500152baf304c20e928ad1a7
KEEWOONG NAM <keewoong.nam@flicspy.com>
2023-07-02 18:33:04 +0900

    Fix

--- modules/s3/output.tf
+++ modules/s3/output.tf
@@ -7,7 +7,7 @@ output "id" {
 }
 
 output "bucket_regional_domain_name" {
-  value = aws_s3_bucket.bucket_regional_domain_name
+  value = aws_s3_bucket.my.bucket_regional_domain_name
 }
 
 output "bucket_domain_name" {
